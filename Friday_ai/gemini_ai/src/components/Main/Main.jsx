c1c7f4cdd8eab78486e6dd285155767ad5bb61c2
kiranoad <oadkiran901@gmail.com>
2025-05-09 14:39:18 +0530

    api is add this commit it well be geminni ai is runing inside of console

--- Friday_ai/gemini_ai/src/components/Main/Main.jsx
+++ Friday_ai/gemini_ai/src/components/Main/Main.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Main.css'
 import { assets } from '../../assets/assets'
+import { Context } from '../../context/context'
 
-function Main() {
+
+const Main = () => {
+
+    const {onSent, recentPrompt,showResult,loading,resultData,input,setInput} =useContext(Context)
+    
+
   return (
     <div>
       <div className="main">
@@ -35,11 +41,12 @@ function Main() {
             </div>
             <div className="main-bottom">
                 <div className="search-box">
-                    <input type="text" placeholder='Enter a prompt here' />
+                <input onChange={(e) => setInput(e.target.value)} value={input} type="text" placeholder="Enter a prompt here" />
+
                     <div>
                         <img src={assets.gallery_icon} alt="" />
                         <img src={assets.mic_icon} alt="" />
-                        <img src={assets.send_icon} alt="" />
+                        <img onClick={()=>onSent()} src={assets.send_icon} alt="" />
                         
                     </div>
                 </div>
